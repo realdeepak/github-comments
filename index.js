0962b6a02fd2722db74c0f66013cea27d20ccae0
Deepak <deepak.kiet@gmail.com>
2023-10-26 11:00:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const makeCommit = (x,y) => {
 //   if(n===0) return simpleGit().push();
 //   const x = random.int(0,54);
 //   const y = random.int(0,6);
-  const DATE = moment().subtract(1,'y').add(1, 'd').format();
+  const DATE = moment().subtract(1,'y').add(1, 'd')
+                .add(x, 'w').add(y, 'd').format();
   const data = {
     date: DATE
   }
