9fd40cd693d15baed3253d4dce511adfd3b42838
Deepak <deepak.kiet@gmail.com>
2023-11-02 15:08:11 +0530

    Fixed - data

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const jsonfile = require('jsonfile');
 const moment = require('moment');
 const simpleGit = require('simple-git');
-//const random= require('random');
+const { generate } = require('random-number-csprng');
+
 const FILE_PATH = './data.json';
-const makeCommit = (x,y) => {
-//   if(n===0) return simpleGit().push();
-//   const x = random.int(0,54);
-//   const y = random.int(0,6);
+const makeCommit = async (n) => {
+  if (n === 0) return await simpleGit().push();
+  const x = getRandomInt(0, 54);
+  const y = getRandomInt(0, 6);
   const DATE = moment().subtract(1,'y').add(1, 'd')
                 .add(x, 'w').add(y, 'd').format();
   const data = {
     date: DATE
   }
-  // console.log(DATE);
+  console.log(DATE);
   jsonfile.writeFile(FILE_PATH, data, () => {
-    simpleGit().add(FILE_PATH).commit(DATE, {'--date': DATE});
+    simpleGit().add(FILE_PATH).commit(DATE, {'--date': DATE},
+    makeCommit.bind(this, --n));
   });
 }
-  makeCommit(3,2);
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+  makeCommit(100);
 
